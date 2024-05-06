5c733cafeee71e7ae6d66e04c2c138425a7388b8
BakeevaIrina <114573318+BakeevaIrina@users.noreply.github.com>
2024-05-06 12:20:08 -0700

    add shows

--- StoryboradTest_IrinaBakeeva/ViewController.swift
+++ StoryboradTest_IrinaBakeeva/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showViewControllerSecond(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "ViewControllerSecond")
+        
+        self.present(viewController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ StoryboradTest_IrinaBakeeva/ViewControllerSecond.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerSecond.swift
+//  StoryboradTest_IrinaBakeeva
+//
+//  Created by Irina Marmyl on 5/6/24.
+//
+
+import UIKit
+
+class ViewControllerSecond: UIViewController {
+
+    @IBAction func shoeViewControllerFirst(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
